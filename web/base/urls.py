0d61acc0c608c835b0e5bdd3cf36168c30e41082
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-03-26 05:22:24 -0500

    reordering url placement

--- web/base/urls.py
+++ web/base/urls.py
@@ -5,9 +5,6 @@ from django.conf.urls import url, patterns, include
 from . import views
 
 urlpatterns = patterns('',
-
-    (r'^accounts/', include('allauth.urls')),
-
     url(r'^$', views.KudoCreate.as_view(), name='kudo_create'),
     url(r'^kudos/$', views.KudoList.as_view(), name='kudo_list'),
     url(r'^kudos/(?P<pk>\d+)$', views.KudoDetail.as_view(), name='kudo_detail'),
--- web/web/urls.py
+++ web/web/urls.py
@@ -5,15 +5,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 
-def bad(request):
-    """ Simulates a server error """
-    1 / 0
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'web.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^bad/$', bad),
+    (r'^accounts/', include('allauth.urls')),
     url(r'', include('base.urls')),
 )
